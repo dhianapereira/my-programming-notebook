dfe7995afdb59e302a83d646b62b2e7ae6b5ac26
dhianapereira <dhianaslv@outlook.com>
2024-03-21 18:15:30 -0300

    feat: Add 01 memory allocation question

---
+++ c_language/data_structure/memory_allocation/01_question.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/utils.h"
+
+int* getNegativeNumbers(int *array, int size, int* count);
+
+int main()
+{
+    int size, count = 0;
+    printf("What the array size?\n");
+    scanf("%d", &size);
+    int* array = (int*)malloc(size * sizeof(int));
+    
+    for(int i=0; i<size; i++){
+        printf("Type the numbers (%d/%d): \n", (i+1), size);
+        scanf("%d", &array[i]);
+    }
+    
+    int *negative_array = getNegativeNumbers(array, size, &count);
+    if(count==0){
+        free(array);
+        printf("There are no negative numbers\n");
+        return 0;
+    }
+
+    printf("The amount of negative number is %d\n", count);
+    printf("Original Array: \n");
+    printingArray(array, size);
+    printf("Negative Array: \n");
+    printingArray(negative_array, count);
+    
+    free(array);
+        
+    return 0;
+}
+
+int* getNegativeNumbers(int *array, int size, int* count) {
+    for(int i=0; i<size; i++){
+        if(array[i] < 0){
+            (*count)++;
+        }
+    }
+
+    int* negative_numbers = (int*)malloc((*count) * sizeof(int));
+    int j = 0;
+    
+    for(int i=0; i<size; i++){
+        if(array[i] < 0){
+            negative_numbers[j++] = array[i];
+        }
+    }
+
+    return negative_numbers;
+}
---
+++ c_language/data_structure/memory_allocation/exercises.md
@@ -0,0 +1,3 @@
+# Exercícios de Alocação de Memória
+
+1. Implemente uma função que receba como parâmetros um vetor de números inteiros de tamanho n, e retorne quantos números negativos estão armazenados nesse vetor e um novo vetor com os valores negativos.
